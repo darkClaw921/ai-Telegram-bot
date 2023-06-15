441615703dfce541ebac0fde930768b4305f4c15
Igor Gerasimov <gerasimov.igor.98@icloud.com>
2023-06-15 17:53:49 +0300

    minor

--- chat.py
+++ chat.py
@@ -34,7 +34,7 @@ class bcolors:
 
 class GPT():
   modelVersion = ''
-  def __init__(self,modelVersion:str = 'gpt-3.5-turbo'):
+  def __init__(self,modelVersion:str = 'gpt-3.5-turbo-16k'):
     self.modelVersion = modelVersion
     pass
 
